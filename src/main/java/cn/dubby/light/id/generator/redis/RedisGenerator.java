79a380b3e3cac7f2270061d27510c0f7102c690c
daoxuan <daoxuan@pinduoduo.com>
2019-07-30 16:14:40 +0800

    timeout

--- src/main/java/cn/dubby/light/id/generator/redis/RedisGenerator.java
+++ src/main/java/cn/dubby/light/id/generator/redis/RedisGenerator.java
@@ -6,6 +6,9 @@ import cn.dubby.light.id.generator.AbstractLightIDGenerator;
 import cn.dubby.light.id.generator.IDProvider;
 import redis.clients.jedis.Jedis;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * @author dubby
  * @date 2019/7/29 16:43
@@ -14,12 +17,12 @@ public class RedisGenerator extends AbstractLightIDGenerator {
 
     private RedisProvider[] providers;
 
-    public RedisGenerator(GeneratorConfig config) {
+    public RedisGenerator(GeneratorConfig config) throws URISyntaxException {
         super(config.getBufferSize(), config.getIdleSize(), config.getCheckInterval());
         providers = new RedisProvider[config.getProviders().size()];
         int index = 0;
         for (GenProviderConfig c : config.getProviders()) {
-            RedisProvider provider = new RedisProvider(c.getId(), new Jedis(c.getUrl()), c.getNamespace());
+            RedisProvider provider = new RedisProvider(c.getId(), new Jedis(new URI(c.getUrl()), config.getTimeout()), c.getNamespace());
             providers[index] = provider;
             ++index;
         }
--- src/test/java/cn/dubby/light/id/test/RedisGeneratorTest.java
+++ src/test/java/cn/dubby/light/id/test/RedisGeneratorTest.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,7 +24,7 @@ public class RedisGeneratorTest {
 
     private static final Logger logger = LoggerFactory.getLogger(RedisGeneratorTest.class);
 
-    public static void main(String[] args) throws LightInitException, InterruptedException, IOException {
+    public static void main(String[] args) throws LightInitException, InterruptedException, IOException, URISyntaxException {
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(ConfigFactory.class.getClassLoader().getResourceAsStream("light_redis.json")));
         String json = reader.lines().reduce((a, b) -> a + b).orElseThrow(() -> new IllegalStateException("load config error"));
